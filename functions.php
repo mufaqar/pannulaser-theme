e677b47215dcc7fea069d4e94bf8b2dd241b4f0c
Mufaqar <mufaqar@gmail.com>
2025-05-12 12:52:34 +0500

    updated

---
+++ functions.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * tp_theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package tp_theme
+ */
+
+ 
+
+ function enqueue_font_awesome() {
+    wp_enqueue_style(
+        'font-awesome',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
+    );
+}
+add_action('wp_enqueue_scripts', 'enqueue_font_awesome');
+
+
+
+	// API 
+
+
+
+
+
+function theme_register_nav_menu() {
+    register_nav_menu('primary', __('Primary Menu', 'theme-textdomain'));
+}
+
+add_action('after_setup_theme', 'theme_register_nav_menu');
+
+
+function my_theme_enqueue_styles() {
+    wp_enqueue_style('tailwindcss', get_template_directory_uri() . '/dist/style.css', array(), null);
+}
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+
+
---
+++ header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+  	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images//favicon.ico" type="image/x-icon">
+	<?php wp_head(); ?>
+</head>
+<?php wp_body_open(); ?>
