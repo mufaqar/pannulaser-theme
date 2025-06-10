dacbc4a33a121bb789d4ef94aa55c0e27c69c1f8
Mufaqar <mufaqar@gmail.com>
2025-06-10 19:39:57 +0500

    Title

--- header.php
+++ header.php
@@ -5,6 +5,32 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="profile" href="https://gmpg.org/xfn/11">
+  <?php if (is_search()) { ?>
+	   <meta name="robots" content="noindex, nofollow" /> 
+	<?php } ?>
+		<title>
+			<?php
+				/*
+				 * Print the <title> tag based on what is being viewed.
+				 */
+				global $page, $paged, $post;
+			
+				wp_title( '|', true, 'right' );
+			
+				// Add the blog name.
+				bloginfo( 'name' );
+			
+				// Add the blog description for the home/front page.
+				$site_description = get_bloginfo( 'description', 'display' );
+				if ( $site_description && ( is_home() || is_front_page() ) )
+					echo " | $site_description";
+			
+				// Add a page number if necessary:
+				if ( $paged >= 2 || $page >= 2 )
+					echo ' | ' . sprintf( __( 'Page %s', 'wpv' ), max( $paged, $page ) );
+            ?>
+	</title>
+    <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico" />
   <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico" type="image/x-icon">
   <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 
