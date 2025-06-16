eb9ae667315834b334bb3345fa150e694baa72ff
Mufaqar <mufaqar@gmail.com>
2025-06-16 15:47:37 +0500

    updated

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <?php /*Template Name: FrontPage*/ get_header(); ?>
 
 <!--Meet-->
-<section class="bg-bgcolor py-20">
+<section class="bg-bgcolor py-32">
     <div class="container mx-auto px-4">
         <div class="flex md:flex-row flex-col items-center md:gap-16 gap-10">
             <!-- Left Text Block -->
@@ -59,7 +59,7 @@
                     </div>
                     <!-- Card 3 -->
                     <div class="about_card ">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/vision.png" alt=""
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/vision.svg" alt=""
                             class='w-[93px] h-[107px] object-contain' />
                         <h3 class="font-bold text-xl text-secondry py-2">100,000 Laser Vision</h3>
                         <p class="text-Pan_black text-base">
@@ -73,11 +73,11 @@
 </section>
 
 <!--Services -->
-<section class="bg-primary py-14">
+<section class="bg-primary pt-[100px] pb-[80px]">
     <div class="container mx-auto px-4">
-        <div class="md:max-w-[60%] w-full mx-auto text-white text-center mb-10">
-            <p class="text-2xl pb-7">Transparent price</p>
-            <h2 class="text-3xl sm:!text-5xl lg:!text-6xl pb-3 font-normal font-axiformaregular">WE PROVIDE BEST <span
+        <div class="md:max-w-[50%] w-full mx-auto text-white text-center mb-10">
+            <p class="text-2xl pb-7 font-normal">Transparent price</p>
+            <h2 class="text-3xl sm:!text-5xl lg:!text-6xl pb-3 font-[300] font-axiformaregular">WE PROVIDE BEST <span
                     class="font-bold font-axiformabold">SERVICES FOR
                     PATIENT</span></h2>
         </div>
@@ -93,21 +93,21 @@
             if ($loop->have_posts()):
                 while ($loop->have_posts()):
                     $loop->the_post(); ?>
-                    <div class="service_card">
-                        <?php
+            <div class="service_card">
+                <?php
                         $services_icon = get_post_meta($post->ID, 'services_icon', true);
                         if ($services_icon) {
                             echo wp_get_attachment_image($services_icon, 'medium', false, ['class' => 'h-[60px] w-[60px]']);
                         }
                         ?>
-                        <div class="font-bold text-xl line-clamp-1 mt-5">
-                            <?php the_title() ?>
-                        </div>
-                        <div class='text-[15px] line-clamp-3 mt-2'>
-                            <?php the_excerpt(); ?>
-                        </div>
-                    </div>
-                <?php endwhile;
+                <div class="font-bold text-xl line-clamp-1 mt-5">
+                    <?php the_title() ?>
+                </div>
+                <div class='text-[15px] line-clamp-3 mt-2'>
+                    <?php the_excerpt(); ?>
+                </div>
+            </div>
+            <?php endwhile;
             else:
                 echo '<p>No doctors found.</p>';
             endif;
@@ -128,18 +128,19 @@
 <!-- See What Our Patients Are Saying -->
 <section class="flex flex-col md:flex-row ">
     <!-- Image Side -->
-    <div class="w-full md:w-1/3">
+    <div class="w-full md:w-1/2">
         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/dentist.png" alt="Dentist"
             class="w-full h-full object-cover">
     </div>
     <!-- Testimonials Side -->
-    <div class="w-full md:w-2/3  px-6 bg-white container mx-auto md:py-16 md:px-10 py-12">
+     <div class='container mx-auto px-4'>
+    <div class="w-full  px-6 bg-white container mx-auto md:py-16  py-12">
         <h2 class="text-3xl sm:text-5xl lg:text-6xl font-bold  pb-6">
             See What Our<br /> Patients Are Saying
         </h2>
 
         <!-- Testimonial Slider -->
-        <div class="testimonial-slider w-full md:-ml-20">
+        <div class="testimonial-slider w-full md:-ml-24">
             <?php
             $args = array(
                 'post_type' => 'testimonials',
@@ -150,24 +151,24 @@
             if ($loop->have_posts()):
                 while ($loop->have_posts()):
                     $loop->the_post(); ?>
-                    <div class="p-4 h-full">
-                        <div
-                            class="bg-white p-6 rounded-tr-[30px] rounded-bl-[30px] shadow-[0_0_15px_0_rgba(0,40,255,0.15)] flex flex-col justify-between h-[388px]">
+            <div class="p-4 h-full">
+                <div
+                    class="bg-white p-6 rounded-tr-[30px] rounded-bl-[30px] shadow-[0_0_15px_0_rgba(0,40,255,0.15)] flex flex-col justify-between h-[388px]">
 
-                            <div class="text-Pan_black text-base italic pt-3 line-clamp-9">
-                                <?php the_content(); ?>
-                            </div>
-                            <div>
-                                <h4 class="font-semibold text-Pan_black text-lg pt-6">
-                                    <?php the_title(); ?>
-                                </h4>
-                                <h4 class="font-semibold text-secondry text-lg">
-                                    <?php echo get_field('position'); ?>
-                                </h4>
-                            </div>
-                        </div>
+                    <div class="text-Pan_black text-base italic pt-3 line-clamp-9">
+                        <?php the_content(); ?>
                     </div>
-                    <?php
+                    <div>
+                        <h4 class="font-semibold text-Pan_black text-lg pt-6">
+                            <?php the_title(); ?>
+                        </h4>
+                        <h4 class="font-semibold text-secondry text-lg">
+                            <?php echo get_field('position'); ?>
+                        </h4>
+                    </div>
+                </div>
+            </div>
+            <?php
                 endwhile;
             else:
                 echo '<p>No testimonials found.</p>';
@@ -178,42 +179,43 @@
         </div>
 
     </div>
+</div>
 </section>
 
 
 
 <!-- See the Difference -->
 
-<section class='bg-primary py-14'>
+<section class='bg-primary pt-[131px] pb-[108px]'>
     <div class='container mx-auto px-4'>
         <h2 class="text-3xl sm:!text-5xl lg:!text-6xl pb-3 text-white text-center">See the Difference</h2>
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8 text-white items-center">
             <!-- card 1 -->
-            <div class="see_card">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Excellence.png" alt="" />
-                <h3 class="font-bold text-secondry text-2xl py-2">Excellence</h3>
-                <p class='text-2xl text-Pan_black'>in Eyecare since 1980</p>
+            <div class="see_card ">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Excellence.svg" alt="" class='w-[70px] h-[82px] ' />
+                <h3 class="font-bold text-secondry text-2xl   py-2">Excellence</h3>
+                <p class='text-2xl text-Pan_black line-clamp-1'>in Eyecare since 1980</p>
             </div>
             <!-- card 2 -->
             <div class="see_card">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Award-winning.png" alt=""
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Award-winning.svg" alt=""
                     class='w-[70px] h-[82px]'>
                 <h3 class="font-bold text-secondry text-2xl py-2">Award winning</h3>
-                <p class='text-2xl text-Pan_black'>Eye specialists</p>
+                <p class='text-2xl text-Pan_black line-clamp-1'>Eye specialists</p>
             </div>
             <!-- card 3 -->
             <div class="see_card">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Empathetic.png" alt=""
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Empathetic.svg" alt=""
                     class='w-[70px] h-[82px]'>
                 <h3 class="font-bold text-secondry text-2xl py-2">Empathetic</h3>
-                <p class='text-2xl text-Pan_black'>Personalized patient care</p>
+                <p class='text-2xl text-Pan_black line-clamp-1'>Personalized patient care</p>
             </div>
             <!-- card 4 -->
             <div class="see_card">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Advanced-care.png" alt=""
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/vision.svg" alt=""
                     class='w-[70px] h-[82px]'>
                 <h3 class="font-bold text-secondry text-2xl py-2">Advanced care</h3>
-                <p class='text-2xl text-Pan_black'>Personalized patient care</p>
+                <p class='text-2xl text-Pan_black line-clamp-1'>Personalized patient care</p>
             </div>
         </div>
     </div>
@@ -222,7 +224,7 @@
 
 <!--  MEET OUR TEAM  -->
 
-<section class='bg-bgcolor py-14'>
+<section class='bg-bgcolor pt-[106px] pb-[105px]'>
     <div class='container mx-auto px-4'>
         <div>
             <div class=" w-full mx-auto text-center">
@@ -231,7 +233,7 @@
                 <h2 class="text-3xl sm:!text-5xl lg:!text-6xl pb-3 text-primary font-axiformaregular">THE MOST
                     QUALIFIED,<br><span class="font-bold font-axiformabold"> SKILLFUL& PROFESSIONAL DOCTORS.</span></h2>
             </div>
-            <div class='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 mt-8 items-center'>
+            <div class='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-8 items-center'>
                 <?php
                 $args = array(
                     'post_type' => 'doctors',
@@ -241,28 +243,31 @@
                 if ($loop->have_posts()):
                     while ($loop->have_posts()):
                         $loop->the_post(); ?>
-                        <div class="bg-white text-center flex flex-col items-center justify-center shadow-lg rounded-md">
-                            <?php
-                            if (has_post_thumbnail()) {
-                                echo '<div style="width:100%; height:350px;">' . get_the_post_thumbnail(get_the_ID(), 'full', ['class' => 'object-cover'], ['style' => 'width:100%; height:350px;']) . '</div>';
-                            }
-                            ?>
-                            <div class='p-4 sm:px-6'>
-                                <div class="font-bold text-Pan_black text-2xl py-2">
-                                    <?php the_title() ?>
-                                </div>
-                                <div class='text-[14px] text-Pan_black line-clamp-3'>
-                                    <?php the_excerpt(); ?>
-                                </div>
-                                <div class="mt-8 text-hovLink text-center flex items-center mx-auto justify-center ">
-                                    <a href="/" class="read-more">MORE
-                                        DETAILS <img
-                                            src="<?php echo get_template_directory_uri(); ?>/assets/images/arrowRight.svg"
-                                            alt="icon" class='w-4'></a>
-                                </div>
-                            </div>
+                <div class="bg-white text-center flex flex-col items-center justify-center shadow-lg rounded-lg ">
+                   <?php
+                         if (has_post_thumbnail()) {
+                         echo get_the_post_thumbnail(get_the_ID(), 'full', [
+                         'class' => 'h-[320px] w-full  !h-[320px]'
+                       ]);
+                      }
+                   ?>
+
+                    <div class='p-4 sm:px-6'>
+                        <div class="font-bold text-Pan_black text-2xl py-2">
+                            <?php the_title() ?>
                         </div>
-                    <?php endwhile;
+                        <div class='text-[14px] text-Pan_black line-clamp-3'>
+                            <?php the_excerpt(); ?>
+                        </div>
+                        <div class="mt-8 text-hovLink text-center flex items-center mx-auto justify-center ">
+                            <a href="/" class="read-more">MORE
+                                DETAILS <img
+                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/arrowRight.svg"
+                                    alt="icon" class='w-4'></a>
+                        </div>
+                    </div>
+                </div>
+                <?php endwhile;
                 else:
                     echo '<p>No doctors found.</p>';
                 endif;
@@ -274,7 +279,7 @@
 </section>
 
 <!-- Locate a Centre -->
-<section class='bg-primary py-14'>
+<section class='bg-primary pt-[85px] pb-[113px]'>
     <div class='container mx-auto px-4'>
         <h2 class="text-3xl sm:!text-5xl lg:!text-6xl pb-3 text-white text-center">Locate a Centre</h2>
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2  text-white items-center">
@@ -374,7 +379,7 @@
     <div class='container mx-auto px-4'>
         <div class='flex flex-col lg:flex-row items-start lg:items-center justify-between gap-8'>
             <!-- Left text content -->
-            <div class='lg:w-[75%]'>
+            <div class='lg:w-[75%] '>
                 <p class='text-2xl md:text-[30px] lg:text-[36px] text-white font-normal'>
                     If you Have Any Questions Schedule an Appointment
                 </p>
