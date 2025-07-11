26c7b4bba58bc785480cc857c90b4733d37e62f9
Mufaqar <mufaqar@gmail.com>
2025-07-11 17:26:42 +0500

    updated

--- temp-locations.php
+++ temp-locations.php
@@ -19,50 +19,51 @@
             $map_link = get_post_meta($post->ID, "map_link", true);
             ?>
 
-           <div class="container mx-auto px-4">
-             <div
-                class="flex md:flex-row flex-col gap-8 items-center bg-[#3F574E] rounded-tl-[25px] rounded-bl-[75px] rounded-tr-[75px] rounded-br-[25px] md:rounded-tr-[75px] shadow-2xl md:pb-0 pb-10">
-                <div class="md:w-2/5 w-full ">
-                    <div class="w-full h-[400px] rounded-tl-[25px] md:rounded-bl-[75px] md:rounded-tr-none rounded-tr-[75px] overflow-hidden">
-                        <iframe src="<?php echo $map_iframe ?>" width="100%" height="100%" style="border:0;" allowfullscreen=""
-                            loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-                        </iframe>
-                    </div>
+    <div class="container mx-auto px-4">
+        <div
+            class="flex md:flex-row flex-col gap-8 items-center bg-[#3F574E] rounded-tl-[25px] rounded-bl-[75px] rounded-tr-[75px] rounded-br-[25px] md:rounded-tr-[75px] shadow-2xl md:pb-0 pb-10">
+            <div class="md:w-2/5 w-full ">
+                <div
+                    class="w-full h-[400px] rounded-tl-[25px] md:rounded-bl-[75px] md:rounded-tr-none rounded-tr-[75px] overflow-hidden">
+                    <iframe src="<?php echo $map_iframe ?>" width="100%" height="100%" style="border:0;"
+                        allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+                    </iframe>
                 </div>
-                <div class="md:w-2/5 w-full space-y-4 md:px-0 px-4">
-                    <h2 class="text-2xl text-white sm:!text-3xl pb-3 font-[300] font-axiformaregular">
-                        <?php the_title() ?>
-                    </h2>
-                    <div class="map_box group">
-                        <span class="map_icon">
-                            <i class="fa-regular fa-map"></i>
-                        </span>
-                        <span class="map_title">
-                            <?php echo $address ?>
-                        </span>
-                    </div>
-                    <div class="map_box group">
-                        <span class="map_icon">
-                            <i class="fa-solid fa-phone"></i>
-                        </span>
-                        <a href="tel:<?php echo $phone ?>" target="_blank" class="map_title">
-                            <?php echo $phone ?>
-                        </a>
-                    </div>
-                    <div class="map_box group ">
-                        <span class="map_icon">
-                            <i class="fa-solid fa-map-pin"></i>
-                        </span>
-                        <a href="<?php echo $map_link ?>" target="_blank" class="map_title">
-                            Get Direction
-                        </a>
-                    </div>
+            </div>
+            <div class="md:w-2/5 w-full space-y-4 md:px-0 px-4">
+                <h2 class="text-2xl text-white sm:!text-3xl pb-3 font-[300] font-axiformaregular">
+                    <a href="<?php the_permalink() ?>"> <?php the_title() ?></a>
+                </h2>
+                <div class="map_box group">
+                    <span class="map_icon">
+                        <i class="fa-regular fa-map"></i>
+                    </span>
+                    <span class="map_title">
+                        <?php echo $address ?>
+                    </span>
                 </div>
-                <div class="md:w-1/5 w-full px-4 space-y-4">
-                    <div class="bg-white p-5 rounded-lg shadow">
-                        <div class="flex items-start gap-2.5">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/google-badge.png" alt="google-badge"
-                            width="32" />
+                <div class="map_box group">
+                    <span class="map_icon">
+                        <i class="fa-solid fa-phone"></i>
+                    </span>
+                    <a href="tel:<?php echo $phone ?>" target="_blank" class="map_title">
+                        <?php echo $phone ?>
+                    </a>
+                </div>
+                <div class="map_box group ">
+                    <span class="map_icon">
+                        <i class="fa-solid fa-map-pin"></i>
+                    </span>
+                    <a href="<?php echo $map_link ?>" target="_blank" class="map_title">
+                        Get Direction
+                    </a>
+                </div>
+            </div>
+            <div class="md:w-1/5 w-full px-4 space-y-4">
+                <div class="bg-white p-5 rounded-lg shadow">
+                    <div class="flex items-start gap-2.5">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/google-badge.png"
+                            alt="google-badge" width="32" />
                         <span class="text-black text-base">
                             Pannu Laser & Vision Institute- Davie
                         </span>
@@ -79,11 +80,11 @@
                     <p>
                         <span class="font-bold text-hovLink">4.8</span> Stars - Based on 321 User Reviews
                     </p>
-                    </div>
                 </div>
             </div>
-           </div>
-            <?php
+        </div>
+    </div>
+    <?php
         endwhile;
 
         // Reset post data
