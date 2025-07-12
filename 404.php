c132cc32f1896210210ad1bf1e4e0f80d959cc45
Mufaqar Islam <mufaqar@gmail.com>
2025-07-12 23:07:01 +0500

    Update 404.php

--- 404.php
+++ 404.php
@@ -12,12 +12,11 @@ get_header();
 
 <main class="min-h-screen bg-gray-100 flex items-center justify-center px-4 py-16">
   <div class="text-center max-w-xl">
-    
     <!-- Image -->
     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/404.png" alt="404 Error" class="w-full  mx-auto ">
 
     <!-- Text -->
-    <p class="text-2xl font-semibold text-gray-800 mb-4">Oops! Page Not Found</p>
+    <p class="text-2xl font-semibold text-gray-800 mb-4">Oops ! Page Not Found</p>
     <p class="text-gray-600 mb-6">The page you are looking for doesn't exist or has been moved.</p>
 
     <!-- Button -->
