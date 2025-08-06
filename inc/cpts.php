1c0d6a28300556cf6a77f8872db9297061523b3a
Mufaqar <mufaqar@gmail.com>
2025-08-06 14:59:06 +0500

    FAQS cpts

--- inc/cpts.php
+++ inc/cpts.php
@@ -187,3 +187,47 @@ function cptui_register_my_taxes() {
 }
 add_action( 'init', 'cptui_register_my_taxes' );
 
+
+function cptui_register_my_cpts_faqs() {
+
+	/**
+	 * Post Type: FAQs.
+	 */
+
+	$labels = [
+		"name" => esc_html__( "FAQs", "tp_theme" ),
+		"singular_name" => esc_html__( "FAQ", "tp_theme" ),
+	];
+
+	$args = [
+		"label" => esc_html__( "FAQs", "tp_theme" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"rest_namespace" => "wp/v2",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"can_export" => false,
+		"rewrite" => [ "slug" => "faqs", "with_front" => true ],
+		"query_var" => true,
+		"supports" => [ "title", "editor", "thumbnail" ],
+		"show_in_graphql" => false,
+	];
+
+	register_post_type( "faqs", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts_faqs' );
+
+
